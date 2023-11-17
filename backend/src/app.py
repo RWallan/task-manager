9eb70aa1c4948b91ac0cfafc3719a7aed91f21bd
RWallan <3am.richardwallan@gmail.com>
2023-11-17 19:40:42 -0300

    feat(api): Incluindo rota users na API

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from backend.src.routers import user_router
+
 app = FastAPI()
 
 
 @app.get("/")
 def health_check():
     return {"status": "online"}
+
+
+app.include_router(user_router)
