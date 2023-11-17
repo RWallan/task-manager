3db04ef20eda67df1d8ed96b54ac4f841930a02a
RWallan <3am.richardwallan@gmail.com>
2023-11-17 18:18:38 -0300

    feat(schema): User Schema

---
+++ backend/src/schemas/user.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+
+class UserBase(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+
+
+class UserCreate(UserBase):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int]
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
