9ef5069e57384e3bb8b119726793efe699e5db2b
RWallan <3am.richardwallan@gmail.com>
2023-11-19 07:41:28 -0300

    update(crud): Aplicando senha criptografada em create e update

--- backend/src/controllers/user.py
+++ backend/src/controllers/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from backend.src.controllers._base import CRUDBase
 from backend.src.database.models import User
 from backend.src.schemas import UserCreate, UserUpdate
+from backend.src.utils.security import Hasher
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
@@ -20,6 +21,21 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             .filter(self.model.username == username)
             .first()
         )
+
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        hashed_obj_in = UserCreate(
+            username=obj_in.username,
+            email=obj_in.email,
+            password=Hasher.get_password_hash(obj_in.password),
+        )
+
+        return super().create(db, obj_in=hashed_obj_in)
+
+    def update(self, db: Session, *, id: int, obj_in: UserUpdate) -> User:
+        if obj_in.password:
+            obj_in.password = Hasher.get_password_hash(obj_in.password)
+
+        return super().update(db, id=id, obj_in=obj_in)
 
 
 user = CRUDUser(User)
