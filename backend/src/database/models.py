494b828dcf5b3ef79f8a7345979c685702bb8a81
RWallan <3am.richardwallan@gmail.com>
2023-11-20 15:29:30 -0300

    feat(table): Tabela tasks

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,4 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from __future__ import annotations
+
+from enum import Enum
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class TaskStatus(str, Enum):
+    draft = "draft"
+    todo = "todo"
+    doing = "doing"
+    done = "done"
+    thrash = "thrash"
 
 
 class Base(DeclarativeBase):
@@ -14,3 +27,19 @@ class User(Base):
     )
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     password: Mapped[str] = mapped_column(index=True, nullable=False)
+
+    tasks: Mapped[list[Task]] = relationship(
+        back_populates="users", cascade="all, delete-orphan"
+    )
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    state: Mapped[TaskStatus] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey(column="users.id"))
+
+    users: Mapped[User] = relationship(back_populates="tasks")
--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,4 +1,4 @@
-from backend.src.database.models import User
+from backend.src.database.models import Task, User
 
 
 def test_create_user(session):
@@ -11,3 +11,17 @@ def test_create_user(session):
     user = session.query(User).where(User.username == "alice").first()
 
     assert user.username == "alice"
+
+
+def test_create_task(session, user):
+    task = Task(
+        title="title", description="desc", state="draft", user_id=user.id
+    )
+
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+
+    user = session.query(User).where(User.id == user.id).first()
+
+    assert task in user.tasks
